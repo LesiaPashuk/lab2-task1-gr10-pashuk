a5030f1d92af8866c039b8b4a3c6fe1a5e61384c
LesiaPashuk <168438305+LesiaPashuk@users.noreply.github.com>
2025-03-24 18:17:22 +0000

    Implement main functionality

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
-/* hello.c */
 #include <stdio.h>
+#include <stdlib.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int main(int argc, char *argv[]) {
+		if(argc < 2) {
+				printf("Usage: %s <size>\n", argv[0]);
+				return 1;
+		}
+
+		int size = atoi(argv[1]);
+		int arr[size];
+
+		// Генерация массива
+		printf("Generated array:\n");
+		for(int i = 0; i < size; i++) {
+				arr[i] = rand() % 100;
+				printf("%d ", arr[i]);
+		}
+		printf("\n");
+
+		// Ваша логика обработки
+
+		return 0;
 }
